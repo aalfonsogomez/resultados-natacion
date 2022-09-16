2a57de7a38a58612ccd200fcbee383f3259798e0
Angel <a.alfonso.gomez@gmail.com>
2022-09-16 15:47:48 +0200

    Corregimos nombre

--- src/tabs/AllTimes.js
+++ src/tabs/AllTimes.js
@@ -1,7 +1,7 @@
 import { Text, View } from 'react-native';
 import useAllTimes from '../hooks/useAllTimes';
 
-export default function Feed() {
+export default function AllTimes() {
     const { allTimes } = useAllTimes();
 
     return (
